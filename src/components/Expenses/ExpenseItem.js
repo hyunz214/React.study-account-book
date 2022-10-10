305d4cd92311a29dfe146f0d13b2834c83806881
HyeonJi Tak <jirae1997@naver.com>
2022-10-10 14:41:42 +0900

    Chart 추가 및 버그 수정

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,22 +4,22 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
+    // const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        setTitle("Update!");
-        console.log(title);
-    };
+    // const clickHandler = () => {
+    //     setTitle("Update!");
+    //     console.log(title);
+    // };
 
     return (
         <li>
             <Card className="expense-item">
                 <ExpenseDate date={props.date}></ExpenseDate>
                 <div className="expense-item__description">
-                    <h2>{title}</h2>
+                    <h2>{props.title}</h2>
                     <div className="expense-item__price">${props.amount}</div>
                 </div>
-                <button onClick={clickHandler}>Change Title</button>
+                {/* <button onClick={clickHandler}>Change Title</button> */}
             </Card>
         </li>
     );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -20,6 +20,7 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            <ExpensesChart expenses={filteredExpenses} />
             <ExpensesList items={filteredExpenses} />
         </Card>
     );
